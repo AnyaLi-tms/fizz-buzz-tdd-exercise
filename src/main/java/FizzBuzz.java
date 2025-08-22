4d3c473c202bc92504d4de1042bc0c1df1bb3126
LIAN18 <lian18@oocl.com>
2025-08-22 15:28:24 +0800

    feat(FizzBuzz): finish BuzzWhizz number

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,6 +4,8 @@ public class FizzBuzz {
             return "FizzBuzz";
         } else if (i % 3 == 0 && i % 7 == 0) {
             return "FizzWhizz";
+        } else if (i % 5 == 0 && i % 7 == 0) {
+            return "BuzzWhizz";
         } else if (i % 5 == 0) {
             return "Buzz";
         } else if (i % 3 == 0) {
