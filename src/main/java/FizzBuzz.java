5e04cd28cb7a1473e6f6b91631a79e001541672c
LIAN18 <lian18@oocl.com>
2025-08-22 15:31:12 +0800

    feat(FizzBuzz): finish FizzBuzzWhizz number

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,8 @@
 public class FizzBuzz {
     public String countOff(int i) {
-        if(i % 3 == 0 && i % 5 == 0) {
+        if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+            return "FizzBuzzWhizz";
+        } else if (i % 3 == 0 && i % 5 == 0) {
             return "FizzBuzz";
         } else if (i % 3 == 0 && i % 7 == 0) {
             return "FizzWhizz";
